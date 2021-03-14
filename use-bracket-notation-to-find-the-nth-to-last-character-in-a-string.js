32f66f004dc528659d12375ddc3b44ba6ff89940
Gabriele Quevedo <54038952+gsquevedo@users.noreply.github.com>
2021-03-14 18:08:50 -0300

    Add files via upload

---
+++ use-bracket-notation-to-find-the-nth-to-last-character-in-a-string.js
@@ -0,0 +1,7 @@
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+
+//Lembre-se de que a posição de qualquer caractere é o comprimento da string, menos um, menos o número de caracteres depois dela .
