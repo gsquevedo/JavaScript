67073d8870ec1833b312e8a9291bd31de288ef0f
Gabriele Quevedo <54038952+gsquevedo@users.noreply.github.com>
2021-03-14 19:28:32 -0300

    Add files via upload

---
+++ comparisons-with-the-logical-and-operator.js
@@ -0,0 +1,20 @@
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25 ) {
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+testLogicalAnd(0);
+testLogicalAnd(24);
+testLogicalAnd(25);
+testLogicalAnd(30);
+testLogicalAnd(50);
+testLogicalAnd(51);
+testLogicalAnd(75);
+testLogicalAnd(80);
