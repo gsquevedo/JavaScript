a5ca4718eaffe05601b602cc49087f5b8592f3ef
Gabriele Quevedo <54038952+gsquevedo@users.noreply.github.com>
2021-06-20 12:53:35 -0300

    Add files via upload

---
+++ remove-items-using-splice.js
@@ -0,0 +1,5 @@
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1,4); //O primeiro parâmetro representa o índice na matriz a partir do qual começar a remover elementos, enquanto o segundo parâmetro indica o número de elementos a serem excluídos.
+// Only change code above this line
+console.log(arr);
